bf9d6233fc079b383977bd99b45a3575daaed901
Andras Kucsma <andras.kucsma@prezi.com>
2015-06-14 12:20:40 +0200

    Add playerCount

--- src/AmobaClient.hpp
+++ src/AmobaClient.hpp
@@ -8,5 +8,8 @@ public:
     using Position = std::tuple<int, int>;
     using Map = std::vector<std::vector<int>>;
 
-    virtual std::tuple<int, int> getMove(const Map& map, int yourColor) = 0;
+    virtual std::tuple<int, int> getMove(
+        const Map& map,
+        int yourColor,
+        int playerCount) = 0;
 };
