bf0a3dcefb0a7c3cb484fcaefdac9fd435e24046
schaumb <schaumb@gmail.com>
2015-06-15 11:56:50 +0200

    weighting kiszervezése

--- src/BelaAmobaClient.hpp
+++ src/BelaAmobaClient.hpp
@@ -1,35 +1,83 @@
 #pragma once
 
+#include <numeric>
+#include <array>
+#include <iostream>
 #include <random>
 #include "AmobaClient.hpp"
 
 class BelaAmobaClient : public AmobaClient
 {
-    struct Weighting
+    struct Info
     {
+        Info(int we) :we(we){}
+        int we;
+        enum class Openness {open = 0, half, closed };
+        enum class Direction {right = 0, down, rightdown, leftdown};
 
-        bool we;
-        int size;
-        int calculate;
-        Weighting(bool we, int size) :we(we), size(size), calculate(we ? 0 : size*size * 2){}
+        // who, size, distance, openness
+        std::array<std::vector<std::tuple<int, int, int, Openness>>, 4> weighting;
 
-        int operator()()
+
+        int calculate(const std::tuple < int, int, int, Openness >& tpl) const
         {
-            return calculate /= 2;
+            // meret * meret
+            // mienk +3
+            // openness +2
+            // tavolsag /2
+            // 
+            return (std::get<1>(tpl) * std::get<1>(tpl) +
+                (std::get<0>(tpl) == we) +
+                (std::get<3>(tpl) == Openness::open ? 2 : 0)) / 
+                std::get<2>(tpl);
+        }
+
+        int calculate(const std::vector < std::tuple < int, int, int, Openness > >& tplv) const
+        {
+            switch (tplv.size())
+            {
+            case 0: return 0;
+            case 1: return calculate(tplv.back());
+            case 2:
+                if (std::get<0>(tplv[0]) != std::get<0>(tplv[1]))
+                {
+                    return std::max(calculate(tplv[0]), calculate(tplv[1]));
+                }
+                else
+                {
+                    return calculate(tplv[0]) + calculate(tplv[1]) - 1;
+                }
+            default: return 0;
+            }
+        }
+
+        void setNewWeight(int who, int size, int distance,  Direction d, Openness openness)
+        {
+            if (distance > 2) return;
+
+            weighting[static_cast<int>(d)].push_back(std::make_tuple(who, size, distance, openness));
+        }
+
+        int getWeight() const
+        {
+            return std::accumulate(weighting.begin(), weighting.end(), 0, 
+                [this](int a, const std::vector<std::tuple<int, int, int, Openness>>& tpl)
+                {
+                    return a + calculate(tpl); 
+                });
         }
     };
+
 public:
     virtual Position getMove(const Map& map, int yourColor, int playerCount)
     {
         int w = map.size();
         int h = map[0].size();
-
-        Map weights(w, std::vector<int>(h, 0));
+        std::vector<std::vector<Info>> weights(w, std::vector<Info>(h, Info(yourColor)));
 
 
         for (int x = 0; x < w; ++x) {
             for (int y = 0; y < h; ++y) {
-
                 int origin = map[x][y];
                 if (origin == 0) {
                     continue;
@@ -47,16 +95,16 @@ public:
 
                         ++countSame;
                     }
-                    Weighting w1(origin == yourColor, countSame);
+
+                    Info::Openness op = static_cast<Info::Openness>((x == 0 || map[x - 1][y] != 0) + (x == w - 1 || map[x + 1][y] != 0));
                     for (int xPrevs = x - 1; xPrevs >= 0 && map[xPrevs][y] == 0; --xPrevs)
                     {
-                        weights[xPrevs][y] += w1();
+                        weights[xPrevs][y].setNewWeight(origin, countSame, std::abs(xPrevs - x), Info::Direction::down, op);
                     }
 
-                    Weighting w2(origin == yourColor, countSame);
                     for (int xNexts = sameEnd; xNexts < w && map[xNexts][y] == 0; ++xNexts)
                     {
-                        weights[xNexts][y] += w2();
+                        weights[xNexts][y].setNewWeight(origin, countSame, xNexts - sameEnd + 1, Info::Direction::down, op);
                     }
                 }
 
@@ -74,16 +122,15 @@ public:
                         ++countSame;
                     }
 
-                    Weighting w1(origin == yourColor, countSame);
+                    Info::Openness op = static_cast<Info::Openness>((y == 0 || map[x][y - 1] != 0) + (y == h - 1 || map[x][y + 1] != 0));
                     for (int yPrevs = y - 1; yPrevs >= 0 && map[x][yPrevs] == 0; --yPrevs)
                     {
-                        weights[x][yPrevs] += w1();
+                        weights[x][yPrevs].setNewWeight(origin, countSame, std::abs(yPrevs - y), Info::Direction::right, op);
                     }
 
-                    Weighting w2(origin == yourColor, countSame);
                     for (int yNexts = sameEnd; yNexts < h && map[x][yNexts] == 0; ++yNexts)
                     {
-                        weights[x][yNexts] += w2();
+                        weights[x][yNexts].setNewWeight(origin, countSame, yNexts - sameEnd + 1, Info::Direction::right, op);
                     }
                 }
 
@@ -101,16 +148,15 @@ public:
                         ++countSame;
                     }
 
-                    Weighting w1(origin == yourColor, countSame);
+                    Info::Openness op = static_cast<Info::Openness>((x == 0 || y == 0 || map[x - 1][y - 1] != 0) + (x == w - 1 || y == h - 1 || map[x + 1][y + 1] != 0));
                     for (int xPrevs = x - 1, yPrevs = y - 1; xPrevs >= 0 && yPrevs >= 0 && map[xPrevs][yPrevs] == 0; --xPrevs,--yPrevs)
                     {
-                        weights[xPrevs][yPrevs] += w1();
+                        weights[xPrevs][yPrevs].setNewWeight(origin, countSame, std::abs(yPrevs - y), Info::Direction::rightdown, op);
                     }
 
-                    Weighting w2(origin == yourColor, countSame);
                     for (int xNexts = sameXEnd, yNexts = sameYEnd; xNexts < w && yNexts < h && map[xNexts][yNexts] == 0; ++xNexts, ++yNexts)
                     {
-                        weights[xNexts][yNexts] += w2();
+                        weights[xNexts][yNexts].setNewWeight(origin, countSame, yNexts - sameYEnd + 1, Info::Direction::rightdown, op);
                     }
                 }
 
@@ -128,38 +174,38 @@ public:
                         ++countSame;
                     }
 
-                    Weighting w1(origin == yourColor, countSame);
+                    Info::Openness op = static_cast<Info::Openness>((x == 0 || y == h - 1 || map[x - 1][y + 1] != 0) + (x == w - 1 || y == 0 || map[x + 1][y - 1] != 0));
                     for (int xPrevs = x - 1, yPrevs = y + 1; xPrevs >= 0 && yPrevs < h && map[xPrevs][yPrevs] == 0; --xPrevs, ++yPrevs)
                     {
-                        weights[xPrevs][yPrevs] += w1();
+                        weights[xPrevs][yPrevs].setNewWeight(origin, countSame, yPrevs - y, Info::Direction::leftdown, op);
                     }
 
-                    Weighting w2(origin == yourColor, countSame);
                     for (int xNexts = sameXEnd, yNexts = sameYEnd; xNexts < w && yNexts >= 0 && map[xNexts][yNexts] == 0; ++xNexts, --yNexts)
                     {
-                        weights[xNexts][yNexts] += w2();
+                        weights[xNexts][yNexts].setNewWeight(origin, countSame, xNexts - sameXEnd + 1, Info::Direction::leftdown, op);
                     }
                 }
             }
         }
-
-        int maxWeight = weights[0][0];
+        int maxWeight = weights[0][0].getWeight();
         std::vector<Position> maxWeights;
-        for (int i = 0; i < w; ++i)
+        for (int j = 0; j < h; ++j)
         {
-            for (int j = 0; j < h; ++j)
+            for (int i = 0; i < w; ++i)
             {
-                if (maxWeight < weights[i][j])
+                std::cout << static_cast<char>('.' + weights[i][j].getWeight());
+                if (maxWeight < weights[i][j].getWeight())
                 {
-                    maxWeight = weights[i][j];
+                    maxWeight = weights[i][j].getWeight();
                     maxWeights.clear();
                     maxWeights.push_back(Position{ i, j });
                 }
-                else if (maxWeight == weights[i][j])
+                else if (maxWeight == weights[i][j].getWeight())
                 {
                     maxWeights.push_back(Position{ i, j });
                 }
             }
+            std::cout << std::endl;
         }
 
         static std::random_device rd;
